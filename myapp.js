7d5590ae448f68643a9cbf31a66e47e1c6fe7513
Sanjul Jain <rr245015@na.ad.rrd.com>
2016-05-25 10:18:13 -0500

    Initial Commit

--- myapp.js
+++ myapp.js
@@ -1,6 +1,8 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+//var http = require('http').Server(app);
+var io = require('socket.io').listen(server);
 
 var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
